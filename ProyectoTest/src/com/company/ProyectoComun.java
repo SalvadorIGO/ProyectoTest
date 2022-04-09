816459d03092a796e008dce23e7f15c95e209134
SalvadorIGO <81437091+SalvadorIGO@users.noreply.github.com>
2022-04-09 19:00:07 -0400

    Propuesta Objetos para el proyecto

---
+++ ProyectoTest/src/com/company/ProyectoComun.java
@@ -0,0 +1,6 @@
+package com.company;
+public class ProyectoComun extends ProyectoAbs{
+    public ProyectoComun(String titulo, String resumen, float duracion, float presupuesto, boolean estado) {
+        super(titulo, resumen, duracion, presupuesto, estado);
+    }
+}
---
+++ ProyectoTest/src/com/company/ProyectoInvestigacion.java
@@ -0,0 +1,6 @@
+package com.company;
+public class ProyectoInvestigacion extends ProyectoAbs {
+    public ProyectoInvestigacion(String titulo, String resumen, float duracion, float presupuesto, boolean estado) {
+        super(titulo, resumen, duracion, presupuesto, estado);
+    }
+}
