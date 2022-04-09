e807d0b0ea37d3c44b6ee9c1a3cdc99eba04d36c
SalvadorIGO <81437091+SalvadorIGO@users.noreply.github.com>
2022-04-09 18:41:29 -0400

    Prueba2

--- ProyectoTest/src/com/company/Main.java
+++ ProyectoTest/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola mundo asies!");
+        System.out.println("Hola mundo asies! test2");
     }
 }
