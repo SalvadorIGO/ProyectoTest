a301a9283621faebab37b21a7e431ab50f0b0eba
SalvadorIGO <81437091+SalvadorIGO@users.noreply.github.com>
2022-04-10 11:00:27 -0400

    Prueba clase presupuesto

--- ProyectoTest/src/com/company/Main.java
+++ ProyectoTest/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola mundoa asies! test2");
+        System.out.println("Hola munadoa asies! test2");
         Presupuesto testP = new Presupuesto(3000,4000,50000);
         ProyectoComun test = new ProyectoComun("Test","Test",3,testP,false);
     }
